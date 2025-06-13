201a683a2f911eb3efbd5ee0df196f008ac631fa
yogadinanta <yogadinanta1@gmail.com>
2025-06-13 11:18:08 +0800

    aupdate

--- index.php
+++ index.php
@@ -1,16 +1,28 @@
 <?php
 require 'conection_db.php';
 
-$result = $conn->query($sql);
 $sql = "SELECT nim, nama, jurusan, angkatan FROM mahasiswa";
+$result = $conn->query($sql);
+
 if ($result->num_rows > 0) {
+    echo "<table border='1' cellpadding='8' cellspacing='0'>";
+    echo "<tr>
+            <th>NIM</th>
+            <th>Nama</th>
+            <th>Jurusan</th>
+            <th>Angkatan</th>
+          </tr>";
+
     while($row = $result->fetch_assoc()) {
-        echo "NIM: " . $row["nim"] .
-             "Nama: " . $row["nama"] . 
-             "Jurusan: " . $row["jurusan"] . 
-             "Angkatan: " . $row["angkatan"] . 
-             "<br>";
+        echo "<tr>
+                <td>" . $row["nim"] . "</td>
+                <td>" . $row["nama"] . "</td>
+                <td>" . $row["jurusan"] . "</td>
+                <td>" . $row["angkatan"] . "</td>
+              </tr>";
     }
+
+    echo "</table>";
 } else {
     echo "Tidak ada data.";
 }
